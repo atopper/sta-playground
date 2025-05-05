6b622a94a6aa28f6c083ed0edad812b63a9d6e6d
Andrew Top <top.od.morning@gmail.com>
2025-05-05 16:28:06 -0400

    Update sta-sp-drive.js

--- .github/actions/sta-sp-drive/sta-sp-drive.js
+++ .github/actions/sta-sp-drive/sta-sp-drive.js
@@ -54,7 +54,7 @@ export async function run() {
   }
 
   if (siteId) {
-    const searchQuery = encodeURIComponent('andrew-top');
+    const searchQuery = encodeURIComponent('name:"andrew-top"');
     const results = await graphFetch(token, `/sites/${siteId}/drive/root/search(q=${searchQuery})`);
     const targetFolders = results.value.filter((item) => item.folder);
     for (const item of targetFolders) {
